6e571e4f1eddfe8b91c94e49b99e671a59856bed
Manuel Selbach <manuel.selbach@udg.de>
2016-05-02 14:14:14 +0200

    [TASK] add old code base

---
+++ ext_tables.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE static_countries (
+  cn_short_ru varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_currencies (
+  cu_name_ru varchar(50) DEFAULT '' NOT NULL,
+  cu_sub_name_ru varchar(20) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_languages (
+  lg_name_ru varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_territories (
+  tr_name_ru varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_taxes (
+  tx_name_ru varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_country_zones (
+  zn_name_ru varchar(50) DEFAULT '' NOT NULL
+);
+
