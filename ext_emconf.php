00b59fe612bfc3991472e09540202e86d10373ca
Manuel Selbach <manuel.selbach@udg.de>
2016-05-02 14:41:33 +0200

    [TASK] reformat code and move files to new extension structure

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,25 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "static_info_tables_ru".
-#
-# Auto generated 28-08-2015 03:21
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Static Info Tables (ru)',
-	'description' => '(ru) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
-	'category' => 'misc',
-	'shy' => 0,
-	'version' => '2.0.0',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '4.0-',
-	'PHP_version' => '4.1.0-',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => 'static_countries,static_languages,static_currencies,static_territories',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Dmitriy Simakov',
-	'author_email' => 'basilio@j-vista.ru',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-);
-
-?>
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Static Info Tables (ru)',
+    'description' => '(ru) language pack for the Static Info Tables providing localized names for countries, 
+                       currencies and so on.',
+    'category' => 'misc',
+    'version' => '3.0.0',
+    'dependencies' => '',
+    'state' => 'stable',
+    'uploadfolder' => true,
+    'createDirs' => '',
+    'clearcacheonload' => true,
+    'modify_tables' => 'static_countries,static_languages,static_currencies,static_territories',
+    'author' => 'Manuel Selbach',
+    'author_email' => 'manuel_selbach@yahoo.de',
+    'author_company' => '',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-7.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
