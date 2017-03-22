bc746f9f3b250cc71ac37cd052250532600ad0d9
Manuel Selbach <manuel_selbach@yahoo.de>
2017-03-22 21:25:08 +0100

    Improvments to TCA overrides

--- Configuration/TCA/Overrides/static_countries.php
+++ Configuration/TCA/Overrides/static_countries.php
@@ -1,24 +1,25 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$LL = 'LLL:EXT:static_info_tables_ru/Resources/Private/Language/locallang_db.xlf:static_countries_item.';
+(function ($extKey) {
+    $LL = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:static_countries_item.';
 
-$additionalFields = [
-    'cn_short_en' => 'cn_short_ru'
-];
-foreach ($additionalFields as $sourceField => $destField) {
-    $additionalColumns = [];
-    $additionalColumns[$destField] = $GLOBALS['TCA']['static_countries']['columns'][$sourceField];
-    $additionalColumns[$destField]['label'] = $LL . $destField;
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('static_countries', $additionalColumns);
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-        'static_countries',
-        $destField,
-        '',
-        'after:' . $sourceField
-    );
-    // Add as search field
-    $GLOBALS['TCA']['static_countries']['ctrl']['searchFields'] .= ',' . $destField;
-}
-unset($additionalColumns);
-unset($additionalFields);
+    $additionalFields = [
+        'cn_short_en' => 'cn_short_ru',
+    ];
+    foreach ($additionalFields as $sourceField => $destField) {
+        $additionalColumns = [];
+        $additionalColumns[$destField] = $GLOBALS['TCA']['static_countries']['columns'][$sourceField];
+        $additionalColumns[$destField]['label'] = $LL . $destField;
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('static_countries', $additionalColumns);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+            'static_countries',
+            $destField,
+            '',
+            'after:' . $sourceField
+        );
+        // Add as search field
+        $GLOBALS['TCA']['static_countries']['ctrl']['searchFields'] .= ',' . $destField;
+    }
+    unset($additionalColumns, $additionalFields);
+})('static_info_tables_ru');
--- Configuration/TCA/Overrides/static_currencies.php
+++ Configuration/TCA/Overrides/static_currencies.php
@@ -1,25 +1,26 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$LL = 'LLL:EXT:static_info_tables_ru/Resources/Private/Language/locallang_db.xlf:static_countries_item.';
+(function ($extKey) {
+    $LL = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:static_countries_item.';
 
-$additionalFields = [
-    'cu_name_en' => 'cu_name_ru',
-    'cu_sub_name_en' => 'cu_sub_name_ru'
-];
-foreach ($additionalFields as $sourceField => $destField) {
-    $additionalColumns = [];
-    $additionalColumns[$destField] = $GLOBALS['TCA']['static_currencies']['columns'][$sourceField];
-    $additionalColumns[$destField]['label'] = $LL . $destField;
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('static_currencies', $additionalColumns);
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-        'static_currencies',
-        $destField,
-        '',
-        'after:' . $sourceField
-    );
-    // Add as search field
-    $GLOBALS['TCA']['static_currencies']['ctrl']['searchFields'] .= ',' . $destField;
-}
-unset($additionalColumns);
-unset($additionalFields);
+    $additionalFields = [
+        'cu_name_en'     => 'cu_name_ru',
+        'cu_sub_name_en' => 'cu_sub_name_ru',
+    ];
+    foreach ($additionalFields as $sourceField => $destField) {
+        $additionalColumns = [];
+        $additionalColumns[$destField] = $GLOBALS['TCA']['static_currencies']['columns'][$sourceField];
+        $additionalColumns[$destField]['label'] = $LL . $destField;
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('static_currencies', $additionalColumns);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+            'static_currencies',
+            $destField,
+            '',
+            'after:' . $sourceField
+        );
+        // Add as search field
+        $GLOBALS['TCA']['static_currencies']['ctrl']['searchFields'] .= ',' . $destField;
+    }
+    unset($additionalColumns, $additionalFields);
+})('static_info_tables_ru');
